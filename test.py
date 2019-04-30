c4d5e9c682499571c57492b162e08f474151a59c
Phil Pemberton <philpem@philpem.me.uk>
2019-04-30 15:30:05 +0100

    Handle heartbeats correctly

--- test.py
+++ test.py
@@ -1,79 +1,143 @@
 #!/usr/bin/env python3
 
+import logging
+import queue
 import socket
 import threading
-import logging
 
 from HyteraADK.packet import *
 
+log = logging.getLogger(__name__)
 
 class ADKSocket(object):
 
     def __init__(self, name="ADKSocket", port=30007, host=''):
+        # Initialise sequence ID and repeater ID
         self.__seq = 0
+        self.__repeaterAddr = None
+
+        # Create the transmit queue
+        self.__txqueue = queue.Queue()
 
         # Open the socket
         self.__sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.__sock.bind((host, port))
 
-        # Start the receive thread
+        # TODO heartbeat timer
+
+        # Create and start the receive and transmit threads
         self.__rxthread = threading.Thread(target=self.__rxThreadProc, name="%s-rx.%d" % (name,port))
+        self.__txthread = threading.Thread(target=self.__txThreadProc, name="%s-tx.%d" % (name,port))
         self.__rxthread.start()
+        self.__txthread.start()
+
+
+    def stop(self):
+        """ Shut down the tx/rx threads """
+        # Shut down the tx thread
+        self.__txqueue.put(None)
+
+        # Shut down the rx thread and join it
+        self.__running = False
+        self.__sock.close()
+        self.__rxthread.join()
+
+
+    def __getSeq(self):
+        """ Get the sequence ID then increment it """
+        x = self.__seq
+        self.__seq += 1
+        return x
+
 
     def __txThreadProc(self):
+        log.debug("TxThread running")
+
+        while True:
+            p = self.__txqueue.get()
+
+            # Quit if there was a null/None in the queue
+            if p is None:
+                log.info("TxThread received None, shutting down...")
+                break
+
+            # Don't allow send if we're not connected to the repeater
+            if self.__repeaterAddr is None:
+                log.warning("Can't send -- not connected to repeater. Packet dropped.")
+                continue
+
+            log.debug("TxThread send: %s" % p)
+
+            # Send the packet
+            self.__sock.sendto(bytes(p), self.__repeaterAddr)
 
 
     def __rxThreadProc(self):
-        l = logging.getLogger("ADKSocket.rxThreadProc")
+        self.__running = True
 
-        # state = "waiting for SYN"
-        state = 0
+        log.debug("RxThread running")
 
-        # Wait for announcement
-        l.debug("RxThread running")
-        while True:
+        while self.__running:
             # Receive a packet
             data,addr = self.__sock.recvfrom(1024)
             p = HYTPacket.decode(data)
-            l.debug("Packet received, addr='%s', data=%s" % (addr, p))
+            log.debug("Packet received, addr='%s', data=%s" % (addr, p))
+
+            # TODO reset the disconnect timer
 
             # Is this a SYN?
+            if isinstance(p, HSTRPSyn):
+                log.debug("SYN... Repeater is id %d, sockaddr %s" % (p.synRepeaterRadioID, addr))
+
+                # Save the repeater address
+                self.__repeaterAddr = addr
+
+                # SYN means we need to reset the sequence id
+                self.__seq = p.hytSeqID
+
+                # Acknowledge the SYN with a SYN-ACK
+                log.debug("   Sending SynAck...")
+                p = HSTRPSynAck()
+                p.hytSeqID = self.__getSeq()
+                self.__txqueue.put(p)
+
+                # At this point, the repeater will begin sending Heartbeat messages
+                #
+                # The repeater will give up and go back to sening SYNs when
+                # it's sent ten heartbeats on a 6-sec interval, without
+                # receiving a heartbeat from us.
+
+            # Is this a Heartbeat?
+            elif isinstance(p, HSTRPHeartbeat):
+                # Sequence ID always seems to be zero
+                # TODO flag the connection as "alive" and reset the connection-death timer?
+                #       (death timer expiry: disconnect repeater and mark as disconnected)
+
+                # FIXME If we have an app crash and restart, the repeater will keep sending
+                #       us Heartbeats, expecting us to reciprocate. We should ignore them
+                #       here until the repeater gives up and goes back to sending SYNs
+
+                # Don't respond to heartbeats if we're not connected
+                if self.__repeaterAddr is None:
+                    log.info("Heartbeat ignored, not connected")
+                    break
+
+                # Respond to a heartbeat with a heartbeat
+                log.debug("   Heartbeat received, responding with a heartbeat...")
+                p = HSTRPHeartbeat()
+                p.hytSeqID = 0      # FIXME really zero?
+                self.__txqueue.put(p)
 
 
-logging.basicConfig(format='%(asctime)s [%(levelname)s] (%(threadName)s) %(message)s', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s [%(levelname)-7s] (%(threadName)s) %(message)s', level=logging.DEBUG)
 
 s = ADKSocket()
 
 
 import time
-time.sleep(120)
+time.sleep(60)
 
-"""
-p = HYTPacket()
+logging.info("Shutting down...")
 
-# print the packet
-print(p)
-# print the bytes
-print(bytes(p))
-# convert bytes back into a packet
-print(HYTPacket(bytes(p)))
-"""
-
-"""
-p = HYTAnnounce()
-print(bytes(p))
-print(p)
-"""
-
-d = b'\x32\x42\x00\x24\x00\x00\x83\x04\x00\x01\x86\x9F\x04\x01\x01\x00\x00\x00'
-p = HYTPacket.decode(d)
-
-print(p)
-print(' '.join(['%02X'%x for x in p.hytPayload]))
-
-print()
-
-p = HYTPacket.decode(bytearray.fromhex('324200050100'))
-print(p)
-print(' '.join(['%02X'%x for x in p.hytPayload]))
+s.stop()
 
