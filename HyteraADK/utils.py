14e384357581a97487c99211e29e71d027a4d764
Phil Pemberton <philpem@philpem.me.uk>
2019-05-09 09:41:16 +0100

    add missing ADK/utils.py

---
+++ HyteraADK/utils.py
@@ -0,0 +1,14 @@
+# ADK - DMR utilities
+
+
+def dmrIPtoID(x):
+    # TODO type check
+    return x & 0xFFFFFF
+
+def dmrIPtoStr(x):
+    a = (x >> 24) & 0xFF
+    b = (x >> 16) & 0xFF
+    c = (x >>  8) & 0xFF
+    d = (x      ) & 0xFF
+    return "%d.%d.%d.%d" % (a,b,c,d)
+
