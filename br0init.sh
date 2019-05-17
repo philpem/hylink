710116ea3158ba2f3e2ca2073d8a072f4de31036
Phil Pemberton <philpem@philpem.me.uk>
2019-05-17 12:50:15 +0100

    br0init: fix iface name (oops)

--- br0init.sh
+++ br0init.sh
@@ -6,5 +6,5 @@ REPEATER=10.0.0.96
 # repeater controller IP
 RPTRCON=10.0.0.230
 
-sudo ip addr add $RPTRCON dev br0 label eth0:1
+sudo ip addr add $RPTRCON dev br0 label br0:1
 sudo ip route add $REPEATER dev br0 src $RPTRCON
